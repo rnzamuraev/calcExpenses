61182967e9fd536f664d19c73bd23bbbb7bf14d4
Ruslan <rnzamuraev@gmail.com>
2022-04-02 19:49:28 +0300

    Do while

--- index.js
+++ index.js
@@ -70,6 +70,28 @@ for (let i = 0; i < 2; i++) {
 //   i++;
 // }
 
+// Цикл do while
+
+// let i = 0;
+// do {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     b = prompt("Во сколько обойдется?", "");
+
+//   if (
+//     typeof a === "string" &&
+//     typeof a != null &&
+//     typeof b != null &&
+//     a != "" &&
+//     b != "" &&
+//     a.length < 30
+//   ) {
+//     console.log("Готово");
+//     appData.expenses[a] = b;
+//     i--;
+//   }
+//   i++;
+// } while (i < 2);
+
 appData.moneyPerDay = appData.budget / 30;
 
 // console.log(expensesSum);
