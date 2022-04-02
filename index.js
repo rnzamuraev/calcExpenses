91b4e92d5a97676e9818db687877cc1084cfdfa1
Ruslan <rnzamuraev@gmail.com>
2022-04-02 19:34:16 +0300

    while

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let money = +prompt("Ваш бюджет на месяц?", ""),
   time = prompt("Введите дату в формате", "YYYY-MM-DD");
 
@@ -34,13 +36,39 @@ for (let i = 0; i < 2; i++) {
     b != "" &&
     a.length < 30
   ) {
-    console.log("done");
+    console.log("Готово");
     appData.expenses[a] = b;
     console.log(appData.expenses);
   } else {
     console.log("Не верно заполнены поля");
+    i--;
   }
 }
+
+// Цикл while
+
+// let i = 0;
+// while (i < 2) {
+//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     b = prompt("Во сколько обойдется?", "");
+
+//   if (
+//     typeof a === "string" &&
+//     typeof a != null &&
+//     typeof b != null &&
+//     a != "" &&
+//     b != "" &&
+//     a.length < 30
+//   ) {
+//     console.log("Готово");
+//     appData.expenses[a] = b;
+//     console.log(appData.expenses);
+//   } else {
+//     console.log("Не верно заполнены поля");
+//     i--;
+//   }
+//   i++;
+// }
 
 appData.moneyPerDay = appData.budget / 30;
 
